1bf402d40ce1e3ffeaeeaafc96ade9f464f35b6f
Pedro Sanders <sanderspedro@gmail.com>
2019-07-01 07:43:51 -0400

    Refactored

--- eet/eet_ims.js
+++ eet/eet_ims.js
@@ -4,22 +4,28 @@ const sleep = require('sleep')
 function test(dutHost, transportMode) {
     const testName = 'UAC to UAS IMS'
 
-    new SIPpW(dutHost)
+    /*new SIPpW(dutHost)
       .withScenario('etc/scenarios/uas_ims.xml')
-        .withTransportMode(transportMode)
-          .startAsync((error, stdout, stderr) => {
-              if (error) {
-                  //console.error(`exec error: ${error}`)
-                  // TODO: End instance after
-                  return
-              }
-          })
+        .withInf('etc/scenarios/uac_register.csv')
+          .withTransportMode(transportMode)
+            .withTimeout(30000)
+              .withOpt('-trace_err', '')
+                .startAsync((error, stdout, stderr) => {
+                    if (error) {
+                        //console.error(error)
+                        // TODO: End instance after
+                        return
+                    }
+                })
+
+    sleep.sleep(5)*/
 
     const result = new SIPpW(dutHost)
       .withScenario('etc/scenarios/uac_ims.xml')
         .withInf('etc/scenarios/uac_ims.csv')
-          .withTransportMode(transportMode)
-            .start()
+          .withOpt('-trace_err', '')
+            .withTransportMode(transportMode)
+              .start()
 
     return { name: testName, result: result }
 }
