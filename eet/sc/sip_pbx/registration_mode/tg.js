32821a30112b64988e30eb95cfb97df67f6b74d0
Pedro Sanders <sanderspedro@gmail.com>
2019-08-14 08:23:39 -0400

    Cleanup

--- eet/sc/sip_pbx/registration_mode/tg.js
+++ eet/sc/sip_pbx/registration_mode/tg.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const { cleanLoc } = require('../../../utils')
 const registration_setup = require('./registration_setup')
 const maintaining_registration = require('./maintaining_registration')
+const authentication = require('./authentication')
 
 function noop(done) { done() }
 
@@ -10,8 +11,8 @@ describe('Test Group 1.1: Registration Mode', function() {
 
     it('SC-IT.Conf.1.1.1: Registration Setup', done => registration_setup(done))
     it('SC-IT.Conf.1.1.2: Registration Failure (Noop)', done => noop(done))
-    it('SC-IT.Conf.1.1.3: Maintaining Registration', done => maintaining_registration(done, this))
-    //it('SC-IT.Conf.1.1.4: Authentication (Noop)', done => noop(done))
-    //it('SC-IT.Conf.1.1.5: TLS Server Mode (Noop)', done => noop(done))
+    it('SC-IT.Conf.1.1.3: Maintaining Registration', done => maintaining_registration(done))
+    it('SC-IT.Conf.1.1.4: Authentication', done => authentication(done))
+    it('SC-IT.Conf.1.1.5: TLS Server Mode (Noop)', done => noop(done))
 
 })
