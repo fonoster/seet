601f237086528081fce2bd262c95b495e94d5a7f
Pedro Sanders <sanderspedro@gmail.com>
2019-08-20 23:12:17 -0400

    Oops!

--- eet/sc/sip_pbx/basic_calling/tg.js
+++ eet/sc/sip_pbx/basic_calling/tg.js
@@ -6,7 +6,7 @@ describe('Test Group 1.3: Basic Call Origination(DOD) and Termination(DID)', fun
 
     after(async () => await cleanLoc())
 
-    it.only('SC-IT.Conf.1.3.1: Verification of INVITE Message Parameters when Originating a DOD Call', done => dod_call_parameters(done))
+    it('SC-IT.Conf.1.3.1: Verification of INVITE Message Parameters when Originating a DOD Call', done => dod_call_parameters(done))
     it('SC-IT.Conf.1.3.2: INVITE Message Processing when Terminating a DID Call', done => noop(done))
     it('SC-IT.Conf.1.3.3: SIPPBX Support for Private when Processing INVITE for Terminating DID Call', done => noop(done))
     it('SC-IT.Conf.1.3.4: SIPPBX Support for Private when Generating INVITE for Originating DOD Call', done => noop(done))
